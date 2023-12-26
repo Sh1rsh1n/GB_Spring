52a8bc83052f52a5be0b83919ac37f157d63b703
dev_hp_laptop <a.s.shirshin@mail.ru>
2023-12-26 23:16:27 +0300

    seminar_1 task is done

--- seminar_1/src/main/java/github/Sh1rsh1n/App.java
+++ seminar_1/src/main/java/github/Sh1rsh1n/App.java
@@ -1,13 +1,33 @@
 package github.Sh1rsh1n;
 
-/**
- * Hello world!
- *
- */
+import com.google.gson.Gson;
+import github.Sh1rsh1n.pojo.Person;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // создаем объект Person
+        Person person = new Person("Alexey", "Shirshin", 37);
+
+        // создаем объект Gson
+        Gson gson = new Gson();
+
+        // сериализуем объект класса Person в JSON-формат, получаем в виде строки
+        String personToJson = gson.toJson(person);
+
+        // десериализуем строку в формате JSON в объект класса Person
+        Person person1 = gson.fromJson(personToJson, Person.class);
+
+        //выводим результат для проверки сериализации в JSON
+        System.out.println(personToJson);
+
+        // проверяем объекты исходный и тот что после десериализации
+        System.out.println(person1.equals(person));
+
+        // объект после десериализации
+        System.out.println(person1);
+
     }
 }
