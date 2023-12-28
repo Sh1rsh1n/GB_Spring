51b117cedb8da345ab63e5c6f4f7114f25d72bba
Alexey Shirshin <110342036+iShAlexei@users.noreply.github.com>
2023-12-28 10:32:36 +0000

    seminar_2 init

---
+++ seminar_2/src/main/java/github/sh1rsh1n/seminar_2/Seminar2Application.java
@@ -0,0 +1,13 @@
+package github.sh1rsh1n.seminar_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar2Application.class, args);
+	}
+
+}
---
+++ seminar_2/src/test/java/github/sh1rsh1n/seminar_2/Seminar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.sh1rsh1n.seminar_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
