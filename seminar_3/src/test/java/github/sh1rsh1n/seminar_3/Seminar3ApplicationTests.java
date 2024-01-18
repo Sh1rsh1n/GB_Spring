485f8326aaf0832165f607e2ff60aaf63592ce87
Sh1rsh1n <95686701+Sh1rsh1n@users.noreply.github.com>
2024-01-18 04:37:53 +0000

    seminar_3 init commit

---
+++ seminar_3/src/test/java/github/sh1rsh1n/seminar_3/Seminar3ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.sh1rsh1n.seminar_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
