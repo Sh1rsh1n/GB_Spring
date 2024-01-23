ee177f73a67002aec35baed0c0b1a59f93f1a2ef
dev_hp_laptop <a.s.shirshin@mail.ru>
2024-01-23 22:19:10 +0300

    seminar_5 init commit

---
+++ seminar_5/src/test/java/github/sh1rsh1n/seminar_5/Seminar5ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.sh1rsh1n.seminar_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
