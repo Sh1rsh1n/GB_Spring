61b02735e377bb4241c391d0d5e2b5de56ec931a
Alexey Shirshin <110342036+iShAlexei@users.noreply.github.com>
2024-01-31 11:35:58 +0000

    seminar_6 commit #007

--- seminar_6/frontend/src/App.js
+++ seminar_6/frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import FormNewNote from './components/FormNewNote';
+import NoteTableView from './components/NoteTableView';
 
-function App() {
+const BASE_URL = 'http://localhost:8080/api/notes';
+
+const App = () => {
+
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get(BASE_URL)
+       .then(res => {
+        const data = [];
+
+        res.data._embedded.notes.forEach(item => {
+          data.push(
+            {
+              id: item.id,
+              title: item.title,
+              description: item.description,
+            }
+          )
+        })
+        setNotes(data);
+      })
+  }, []);
+
+  const appendNote = (title, description) => {
+
+    const temp = {
+      // id: currentId,
+      title: title,
+      description: description,
+    };
+
+    axios.post(BASE_URL, temp)
+    .then(e => {
+      temp.id = e.data.id;
+      setNotes([...notes, temp]);
+    });
+  }
+
+  const removeNote = (id) => {
+    axios.get(BASE_URL + '/delete/{id}');
+    setNotes(notes.filter(note => note.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <div className='card'>
+        <div className='card-header'>
+          <h1>Note List</h1>
+        </div>
+        <div className='card-body'>
+          <NoteTableView data={notes} removeNote={removeNote} />
+          <FormNewNote appendNote={appendNote} />
+        </div>
+      </div>
     </div>
   );
 }
