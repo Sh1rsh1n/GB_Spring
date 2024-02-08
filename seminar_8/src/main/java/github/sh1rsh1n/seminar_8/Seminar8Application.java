83b43dd04acf23ddaa25c835f2c8b23e3df2892d
Alexey Shirshin <110342036+iShAlexei@users.noreply.github.com>
2024-02-08 11:52:54 +0000

    seminar_8 commit #002

--- seminar_8/src/main/java/github/sh1rsh1n/seminar_8/Seminar8Application.java
+++ seminar_8/src/main/java/github/sh1rsh1n/seminar_8/Seminar8Application.java
@@ -1,29 +1,13 @@
 package github.sh1rsh1n.seminar_8;
 
-import github.sh1rsh1n.seminar_8.entity.Role;
-import github.sh1rsh1n.seminar_8.entity.User;
-import github.sh1rsh1n.seminar_8.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-public class Seminar8Application implements CommandLineRunner {
+public class Seminar8Application {
 
-    private final UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Seminar8Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        User user = new User("user", "$2a$10$jQlBlZZuO4aTHDw2YXwimupl0GrDGZRPCjsAr/eDFBT3AWRmBPaqe", Role.USER);
-        userRepository.save(user);
-
-        User admin = new User("admin", "$2a$10$SN6IVfmAGy0fiRd5FsM9tOcgQLpvZvLHtbwbAEJ5zA0Nxe9ILsvf2", Role.ADMIN);
-        userRepository.save(admin);
     }
 }
